b79793f0238d04f6a9f830b182578fcc6527509f
mike_b <atm89@hotmail.com>
2016-06-17 12:58:34 +0200

    splash screen
    level loading added and music

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+	public void LoadLevel(string nameLevel)
+    {
+        SceneManager.LoadScene(nameLevel);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+	public void QuitRequest()
+    {
+		//Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    public AudioClip[] levelMusicChangeArray;
+    private AudioSource audioSource;
+
+    void Awake()
+    {        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+	void OnLevelWasLoaded (int level)
+    {
+        AudioClip thisLevelsMusic = levelMusicChangeArray[level];
+        //level = SceneManager.GetActiveScene().buildIndex +1;
+
+        if (thisLevelsMusic)
+        {
+            audioSource.clip = thisLevelsMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+	}
+}
