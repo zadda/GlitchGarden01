4799cd7349eb86b94146af9a8abac22031db0f78
mike_b <atm89@hotmail.com>
2016-06-17 15:45:34 +0200

    add fade script
    fade in start screen, alpha channel

---
+++ Assets/Fade.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
